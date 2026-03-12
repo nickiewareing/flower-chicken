5ffc417718a37ad1a253b06079b3ae98e724e041
nickie.wareing1 <nickie.wareing1@nhs.net>
2026-03-12 16:52:24 +0000

    made functions ugly to test linter action

--- src/basic_functions.py
+++ src/basic_functions.py
@@ -1,10 +1,20 @@
+from typing     import List
+
 # these functions are a load of nothing
 # just want to test github actions
 
 
-def return_greeting(name: str, greeting: str = 'Hi'):
-    print(f"Hi {name}")
+def return_greeting(
+        name: str, greeting: str = 'Hi'):
+    print(
+        
+        f"{greeting} {name}")
 
 
-def add_numbers_in_list(nums):
-    return sum(nums)
+def add_numbers_in_list(nums: List[str]):
+    return sum(
+        
+        
+        
+        
+        nums)
