82a6c309728126847daca1451f1b1557362c0648
nickie.wareing1 <nickie.wareing1@nhs.net>
2026-03-13 16:07:22 +0000

    correct answer in test - should all pass now

--- tests/test_rubbish_functions.py
+++ tests/test_rubbish_functions.py
@@ -7,4 +7,4 @@ def test_return_greeting():
 
 def test_sum():
     nums = [1, 2, 3]
-    assert basic_functions.add_numbers_in_list(nums) == 3
+    assert basic_functions.add_numbers_in_list(nums) == 6
